0c8739684a4fe632f69d23d487adc8e234f931eb
Kelly Webber <kwebber@mica.edu>
2015-11-02 16:35:38 -0500

    Initial Check-in

---
+++ Styleguide/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Pattern Primer</title>
+<link rel="stylesheet" href="global.css">
+<link rel="stylesheet" href="div.css">
+<style>
+.pattern {
+    clear: both;
+    overflow: hidden;
+}
+.pattern .display {
+    max-width: 1248px;
+    width: 65%;
+    float: left;
+}
+.pattern .source {
+    width: 30%;
+    float: right;
+}
+.pattern .source textarea {
+    width: 90%;
+}
+</style>
+</head>
+<body>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<?php
+$files = array();
+$handle=opendir('patterns');
+while (false !== ($file = readdir($handle))):
+    if(substr($file, -5) == '.html'):
+        $files[] = $file;
+    endif;
+endwhile;
+sort($files);
+foreach ($files as $file):
+    echo '<div class="pattern">';
+    echo '<div class="display">';
+    include('patterns/'.$file);
+    echo '</div>';
+    echo '<div class="source">';
+    echo '<textarea rows="6" cols="30">';
+    echo htmlspecialchars(file_get_contents('patterns/'.$file));
+    echo '</textarea>';
+    echo '<p><a href="patterns/'.$file.'">'.$file.'</a></p>';
+    echo '</div>';
+    echo '</div>';
+endforeach;
+?>
+<br/><br/><br/><br/>
+</body>
+</html>
